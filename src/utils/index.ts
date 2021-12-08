178011104e1d153c034536779b3453c90184f1b2
Makar Kotlov <m.kotlov@qlean.ru>
2021-12-08 22:23:01 +0300

    add utils

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export function randomBetween(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export function getObjectKeys<O>(object: O) {
+  return Object.keys(object) as (keyof O)[]
+}
