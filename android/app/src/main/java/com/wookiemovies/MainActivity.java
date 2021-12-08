0b6aa2e65766c2bc630320e3056b7486fc2e1b61
Makar Kotlov <m.kotlov@qlean.ru>
2021-12-08 22:18:21 +0300

    configure android native parts

--- android/app/src/main/java/com/wookiemovies/MainActivity.java
+++ android/app/src/main/java/com/wookiemovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wookiemovies;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -10,6 +11,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "wookieMovies";
+    return "wookiemovies";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
