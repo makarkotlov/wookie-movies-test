94a3809ae6ca4bd915a1805fe23aab3160e198ea
Makar Kotlov <m.kotlov@qlean.ru>
2021-12-08 22:22:37 +0300

    add hooks

---
+++ src/hooks/style.ts
@@ -0,0 +1,12 @@
+import { DependencyList, useMemo } from 'react'
+import { StyleSheet, StyleProp } from 'react-native'
+
+function compileStyle<T>(style: T) {
+  return StyleSheet.create({ style }).style
+}
+
+export const useStyle = <T>(provider: () => T, dependencies?: DependencyList) =>
+  useMemo(() => compileStyle<T>(provider()), dependencies)
+
+export const useFlattenStyle = <Style>(provider: () => StyleProp<Style>, dependencies?: DependencyList) =>
+  useStyle<Style>(() => StyleSheet.flatten<Style>(provider()) as Style, dependencies)
