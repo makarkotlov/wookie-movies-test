45eae0c69daeded289f63c9d5aaf7e24cdbff540
Makar Kotlov <m.kotlov@qlean.ru>
2021-12-08 22:19:24 +0300

    rename wookieMovies to wookiemovies

--- ios/wookieMoviesTests/wookieMoviesTests.m
+++ ios/wookieMoviesTests/wookieMoviesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface wookieMoviesTests : XCTestCase
+@interface wookiemoviesTests : XCTestCase
 
 @end
 
-@implementation wookieMoviesTests
+@implementation wookiemoviesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
