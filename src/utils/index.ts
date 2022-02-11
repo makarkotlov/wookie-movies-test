63442d3e42244875be9cd54504528a3d04c6c5dc
Makar Kotlov <m.kotlov@qlean.ru>
2022-02-11 21:04:23 +0300

    delete unused utils

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function randomBetween(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-export function getObjectKeys<O>(object: O) {
-  return Object.keys(object) as (keyof O)[]
-}
